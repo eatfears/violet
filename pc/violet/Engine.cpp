573c8b4d8b5c8a0de2f7d0419d636750a25dbdd0
Ilya Sivakov <ilyasivakov@gmail.com>
2013-02-13 03:54:33 +0400

    HUD numbers

--- pc/violet/Engine.cpp
+++ pc/violet/Engine.cpp
@@ -235,54 +235,122 @@ void Engine::DrawInterface()
 	glDisable(GL_BLEND);
 	glColor3f(1.0, 1.0, 1.0);
 	//HUD
+
+	float s = 8.;
+	float r = 5.;
+
+	int pixPerDeg = 20.5;
+
+	int sizline = 120;
+	int sizline2 = 70;
+	int sizline3 = 20;
+	int sizline4 = 10;
+	static char temp_text[1024] = "";
+
 	glLoadIdentity();
 
-	glLineWidth (1.5);
+	glLineWidth (1.);
 
 	glTranslated(WidthBy2,HeightBy2,0);
-	static float s = 0;
 
-	int pixPerDeg = 10.5;
+	s = (M_PI * 2 / s);
+	glBegin(GL_LINE_LOOP);    
+	for(float i = M_PI; i >= -M_PI; i -= s)
+		glVertex2f(cos(i) * r, sin(i) * r);
+	glEnd();
+
+	glBegin(GL_LINES);    
+	glVertex2f(r, 0);
+	glVertex2f(sizline3, 0);
+	glVertex2f(-r, 0);
+	glVertex2f(-sizline3, 0);
+	glVertex2f(0, r);
+	glVertex2f(0, sizline3);
+	glVertex2f(0, -r);
+	glVertex2f(0, -sizline3);
+
+	glEnd();
+
+
 	
 	glRotatef(TODEG(inter.quadroRoll),0,0,1);
 	glTranslatef(0,TODEG(inter.quadroPitch)*pixPerDeg,0);
 	
-	s+= 0.1;
-	int sizline = 100;
-	int sizline2 = 70;
-	int sizline3 = 20;
-	int sizline4 = 10;
+
+
 
 	int siz = (sizline2-sizline3)/5;
 
+	glPushMatrix();
+	//	for (int deg = -360; deg <= 360; deg += 5)
+	for(int deg = -180; deg <= 180; deg += 5)
+	{
+		glPushMatrix();
+		if(deg >= -90 && deg <= 90) b_sprintf(temp_text, "%d", deg);
+		if(deg > 90) b_sprintf(temp_text, "%d", 180 - deg);
+		if(deg < -90) b_sprintf(temp_text, "%d", -180 - deg);
+
+		glPushMatrix();
+			if(deg == 0 || deg == 90 || deg == -90) glTranslated(sizline*1.1,deg*pixPerDeg,0);
+			else glTranslated(sizline2*1.1,deg*pixPerDeg,0);
+			//if(deg >= 0) 
+			glTranslated(0,-sizline4/2,0);
+
+			glScalef(0.1, 0.1, 0.1);
+			glutStrokeString(GLUT_STROKE_ROMAN, (unsigned char*) temp_text);
+		glPopMatrix();
+			if(deg == 0 || deg == 90 || deg == -90) glTranslated(-sizline*1.1,deg*pixPerDeg,0);
+			else glTranslated(-sizline2*1.1,deg*pixPerDeg,0);
+			glTranslated(-(float)strlen(temp_text)*10.0,0,0);
+			//if(deg >= 0) 
+			glTranslated(0,-sizline4/2,0);
+
+
+			glScalef(0.1, 0.1, 0.1);
+			glutStrokeString(GLUT_STROKE_ROMAN, (unsigned char*) temp_text);
+		glPopMatrix();
+	}
+	
+	glPopMatrix();
+
 	glBegin(GL_LINES);
 
-	for (int deg = -360; deg <= 360; deg += 5)
+	for (int deg = -180; deg <= 180; deg += 5)
 	{
 		int h = deg*pixPerDeg;
-		if(deg == 0 || deg == 360 || deg == -360) {
+
+		if(deg == 0 || deg == 90 || deg == -90) {
 			glVertex2f(-sizline, h);
 			glVertex2f(-sizline3, h);
 			glVertex2f(sizline, h);
 			glVertex2f(sizline3, h);
-
-			glVertex2f(-sizline, h);
-			glVertex2f(-sizline, h-sizline4);
-			glVertex2f(sizline, h);
-			glVertex2f(sizline, h-sizline4);
-
+			if(deg == 0)
+			{
+				glVertex2f(-sizline, h);
+				glVertex2f(-sizline, h-sizline4);
+				glVertex2f(sizline, h);
+				glVertex2f(sizline, h-sizline4);
+			}
 		} else if (deg > 0){
 			glVertex2f(-sizline2, h);
 			glVertex2f(-sizline3, h);
 			glVertex2f(sizline2, h);
 			glVertex2f(sizline3, h);
 
-			glVertex2f(-sizline2, h);
-			glVertex2f(-sizline2, h-sizline4);
-			glVertex2f(sizline2, h);
-			glVertex2f(sizline2, h-sizline4);
+			if(deg < 90) {
+				glVertex2f(-sizline2, h);
+				glVertex2f(-sizline2, h-sizline4);
+				glVertex2f(sizline2, h);
+				glVertex2f(sizline2, h-sizline4);
+			} else {
+				glVertex2f(-sizline2, h);
+				glVertex2f(-sizline2, h+sizline4);
+				glVertex2f(sizline2, h);
+				glVertex2f(sizline2, h+sizline4);
+			}
 
-		} else if (deg < 0){
+
+		} else if(deg < 0) {
 			glVertex2f(-sizline2, h);
 			glVertex2f(-sizline2 + siz, h);
 			glVertex2f(-sizline2 + siz*2, h);
@@ -296,10 +364,17 @@ void Engine::DrawInterface()
 			glVertex2f(sizline2 - siz*4, h);
 			glVertex2f(sizline2 - siz*5, h);
 
-			glVertex2f(-sizline2, h);
-			glVertex2f(-sizline2, h+sizline4);
-			glVertex2f(sizline2, h);
-			glVertex2f(sizline2, h+sizline4);
+			if(deg < -90) {
+				glVertex2f(-sizline2, h);
+				glVertex2f(-sizline2, h-sizline4);
+				glVertex2f(sizline2, h);
+				glVertex2f(sizline2, h-sizline4);
+			} else {
+				glVertex2f(-sizline2, h);
+				glVertex2f(-sizline2, h+sizline4);
+				glVertex2f(sizline2, h);
+				glVertex2f(sizline2, h+sizline4);
+			}
 		}
 	}
 	
